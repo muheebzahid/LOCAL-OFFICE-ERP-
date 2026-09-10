bc669ccb26d8fc82f67bc829af8f848c4c395c86
Muheeb Zahid <muheebzahid@gmail.com>
2026-09-11 02:34:29 +0400

    Add compiled production build

---
+++ .next/static/chunks/app/page-3461a632c402be0e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{6708:(e,s,_)=>{Promise.resolve().then(_.bind(_,9809))}},e=>{var s=s=>e(e.s=s);e.O(0,[553,809,441,587,358],()=>s(6708)),_N_E=e.O()}]);
